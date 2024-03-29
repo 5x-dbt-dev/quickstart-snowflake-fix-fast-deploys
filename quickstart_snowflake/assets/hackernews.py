42dd1168a7df6f3137a1f20f7672a927b24f92e9
anurag-5x <157478686+anurag-5x@users.noreply.github.com>
2024-03-29 09:31:54 +0530

    Update hackernews.py with snowflake credentials

--- quickstart_snowflake/assets/hackernews.py
+++ quickstart_snowflake/assets/hackernews.py
@@ -6,32 +6,48 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 from wordcloud import STOPWORDS, WordCloud
-
 from dagster import MetadataValue, OpExecutionContext, asset
+from dagster_snowflake import snowflake_io_manager
 
+snowflake_creds = {
+    "account": "https://rb83340.europe-west4.gcp.snowflakecomputing.com/",
+    "user": "ranjuramesh",
+    "password": "NpI9UcA8Cn#9",
+    "warehouse": "compute_wh",
+    "database": "rawdata_db",
+    "schema": "testing",
+}
 
-@asset(group_name="hackernews", compute_kind="HackerNews API")
+snowflake_io_manager = snowflake_io_manager.configured(snowflake_creds)
+
+@asset(
+    group_name="hackernews",
+    compute_kind="HackerNews API",
+    key_prefix=["hackernews"],
+    io_manager_key="snowflake_io_manager",
+)
 def hackernews_topstory_ids() -> pd.DataFrame:
     """
     Get up to 500 top stories from the HackerNews topstories endpoint.
-
     API Docs: https://github.com/HackerNews/API#new-top-and-best-stories
     """
     newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
     top_500_newstories = requests.get(newstories_url).json()
     return pd.DataFrame(top_500_newstories, columns=["item_ids"])
 
-
-@asset(group_name="hackernews", compute_kind="HackerNews API")
+@asset(
+    group_name="hackernews",
+    compute_kind="HackerNews API",
+    key_prefix=["hackernews"],
+    io_manager_key="snowflake_io_manager",
+)
 def hackernews_topstories(
     context: OpExecutionContext, hackernews_topstory_ids: pd.DataFrame
 ) -> pd.DataFrame:
     """
     Get items based on story ids from the HackerNews items endpoint. It may take 1-2 minutes to fetch all 500 items.
-
     API Docs: https://github.com/HackerNews/API#items
     """
-
     results = []
     for item_id in hackernews_topstory_ids["item_ids"]:
         item = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json").json()
@@ -41,18 +57,14 @@ def hackernews_topstories(
 
     df = pd.DataFrame(results)
 
-    # Dagster supports attaching arbitrary metadata to asset materializations. This metadata will be
-    # shown in the run logs and also be displayed on the "Activity" tab of the "Asset Details" page in the UI.
-    # This metadata would be useful for monitoring and maintaining the asset as you iterate.
-    # Read more about in asset metadata in https://docs.dagster.io/concepts/assets/software-defined-assets#recording-materialization-metadata
     context.add_output_metadata(
         {
             "num_records": len(df),
             "preview": MetadataValue.md(df.head().to_markdown()),
         }
     )
-    return df
 
+    return df
 
 @asset(group_name="hackernews", compute_kind="Plot")
 def hackernews_topstories_word_cloud(
@@ -61,26 +73,22 @@ def hackernews_topstories_word_cloud(
     """
     Exploratory analysis: Generate a word cloud from the current top 500 HackerNews top stories.
     Embed the plot into a Markdown metadata for quick view.
-
     Read more about how to create word clouds in http://amueller.github.io/word_cloud/.
     """
     stopwords = set(STOPWORDS)
     stopwords.update(["Ask", "Show", "HN"])
+
     titles_text = " ".join([str(item) for item in hackernews_topstories["title"]])
     titles_cloud = WordCloud(stopwords=stopwords, background_color="white").generate(titles_text)
 
-    # Generate the word cloud image
     plt.figure(figsize=(8, 8), facecolor=None)
     plt.imshow(titles_cloud, interpolation="bilinear")
     plt.axis("off")
     plt.tight_layout(pad=0)
 
-    # Save the image to a buffer and embed the image into Markdown content for quick view
     buffer = BytesIO()
     plt.savefig(buffer, format="png")
     image_data = base64.b64encode(buffer.getvalue())
     md_content = f"![img](data:image/png;base64,{image_data.decode()})"
 
-    # Attach the Markdown content as metadata to the asset
-    # Read about more metadata types in https://docs.dagster.io/_apidocs/ops#metadata-types
     context.add_output_metadata({"plot": MetadataValue.md(md_content)})
